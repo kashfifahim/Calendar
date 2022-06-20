4ab8c35a2ff4ca4fa3d62eb836b36fa29de0e723
Kashfi <kashfi.fahim74@qmail.cuny.edu>
2022-06-20 12:14:10 -0400

    Test Entry class in Main.py

---
+++ Main.py
@@ -0,0 +1,9 @@
+'''Main Class'''
+'''written by Kashfi Fahim'''
+'''kashfi.fahim@gmail.com'''
+
+from Entries import Entry
+
+'''Testing instantiation of an Entry'''
+e01 = Entry('KashfiCon', '2022-05-22', '2022-05-23', 'New York', 'USA', '2022-04-04', '2022-04-01')
+e01.show()
